57dc3799eac07e16521d03edbb733469381a7aa0
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-12-22 11:05:44 +0700

    feat(proxy): add bot detection and forbidden access
    Added bot detection and forbidden access logic to the proxy server. The `/cf` and `/<userID>` routes now check the user-agent header for bot patterns and respond accordingly. If the request is identified as a bot, a forbidden response with status code 403 is returned. Additionally, random headers are set for the requests made to different domains, and redirects to specific domains are disallowed, resulting in a forbidden response.
    
    This commit aims to improve security and prevent unauthorized access from bots while providing reliable proxy functionality.

--- README.md
+++ README.md
@@ -16,15 +16,7 @@ Cloudflare pure IP update: [https://t.me/edtunnel](https://t.me/edtunnel)
 
    [https://www.youtube.com/watch?v=8I-yTNHB0aw](https://www.youtube.com/watch?v=8I-yTNHB0aw)
 
-2. Clone this repository deploy in cloudflare pages.
-
-## Deploy in worker.dev
-
-1. Copy `_worker.js` code from [here](https://github.com/3Kmfi6HP/EDtunnel/blob/main/_worker.js).
-
-2. Alternatively, you can click the button below to deploy directly.
-
-   [![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/3Kmfi6HP/EDtunnel)
+2. Clone this repository deploy in Cloudflare Pages.
 
 ## Lazy to deploy
 
@@ -32,11 +24,9 @@ Cloudflare pure IP update: [https://t.me/edtunnel](https://t.me/edtunnel)
 
 ## UUID Setting (Optional)
 
-1. When deploy in cloudflare pages, you can set uuid in `wrangler.toml` file. variable name is `UUID`. `wrangler.toml` file is also supported. (recommended) in case deploy in webpages, you can not set uuid in `wrangler.toml` file.
+1. When deploy in Cloudflare Pages, you can set uuid in `wrangler.toml` file. variable name is `UUID`. (recommended) in case deploy in webpages, you can not set uuid in webpages.
 
-2. When deploy in worker.dev, you can set uuid in `_worker.js` file. variable name is `userID`. `wrangler.toml` file is also supported. (recommended) in case deploy in webpages, you can not set uuid in `wrangler.toml` file. in this case, you can also set uuid in `UUID` enviroment variable.
-
-Note: `UUID` is the uuid you want to set. pages.dev and worker.dev all of them method supported, but depend on your deploy method.
+Note: `UUID` is the uuid you want to set. Cloudflare Pages supports this method, but it depends on your deploy method.
 
 ### UUID Setting Example
 
@@ -46,14 +36,14 @@ Note: `UUID` is the uuid you want to set. pages.dev and worker.dev all of them m
    UUID = "uuid here your want to set"
    ```
 
-2. multiple uuid environment variable
+<!-- 2. multiple uuid environment variable
 
    ```.environment
    UUID = "uuid1,uuid2,uuid3"
    ```
 
    note: uuid1, uuid2, uuid3 are separated by commas`,`.
-   when you set multiple uuid, you can use `https://edtunnel.pages.dev/uuid1` to get the clash config and vless:// link.
+   when you set multiple uuid, you can use `https://edtunnel.pages.dev/uuid1` to get the clash config and vless:// link. -->
 
 ## subscribe vless:// link (Optional)
 
@@ -61,21 +51,21 @@ Note: `UUID` is the uuid you want to set. pages.dev and worker.dev all of them m
 
 2. visit `https://edtunnel.pages.dev/sub/uuid your set` to get the subscribe content with `uuid your set` path.
 
-   note: `uuid your set` is the uuid you set in UUID enviroment or `wrangler.toml`, `_worker.js` file.
+   note: `uuid your set` is the uuid you set in UUID enviroment or `wrangler.toml` file.
    when you set multiple uuid, you can use `https://edtunnel.pages.dev/sub/uuid1` to get the subscribe content with `uuid1` path.(only support first uuid in multiple uuid set)
 
 3. visit `https://edtunnel.pages.dev/sub/uuid your set/?format=clash` to get the subscribe content with `uuid your set` path and `clash` format. content will return with base64 encode.
 
-   note: `uuid your set` is the uuid you set in UUID enviroment or `wrangler.toml`, `_worker.js` file.
+   note: `uuid your set` is the uuid you set in UUID enviroment or `wrangler.toml` file.
    when you set multiple uuid, you can will use `https://edtunnel.pages.dev/sub/uuid1/?format=clash` to get the subscribe content with `uuid1` path and `clash` format.(only support first uuid in multiple uuid set)
 
-## subscribe Cloudflare bestip(pure ip) link
+## subscribe Cloudflare bestip(pure IP) link
 
 1. visit `https://edtunnel.pages.dev/bestip/uuid your set` to get subscribe info.
 
-2. cpoy subscribe url link `https://edtunnel.pages.dev/bestip/uuid your set` to any clients(clash/v2rayN/v2rayNG) you want to use.
+2. cpoy subscribe URL link `https://edtunnel.pages.dev/bestip/uuid your set` to any clients(clash/v2rayN/v2rayNG) you want to use.
 
-3. done. if have any questions please join [@edtunnel](https://t.me/edtunnel)
+3. done. if you have any questions, please join [@edtunnel](https://t.me/edtunnel)
 
 ## multiple port support (Optional)
 
@@ -92,22 +82,20 @@ http port: 80, 8080, 8880, 2052, 2086, 2095
 https port: 443, 8443, 2053, 2096, 2087, 2083
 ```
 
-if you deploy in cloudflare pages, https port is not supported. Simply add multiple ports node drictly use subscribe link, subscribe content will return all Cloudflare supported ports.
+if you deploy in Cloudflare Pages, https port is not supported. Simply add multiple ports node directly to use the subscribe link, the subscribe content will return all Cloudflare supported ports.
 
 ## proxyIP (Optional)
 
-1. When deploy in cloudflare pages, you can set proxyIP in `wrangler.toml` file. variable name is `PROXYIP`.
+1. When deploy in Cloudflare Pages, you can set proxyIP in `wrangler.toml` file. variable name is `PROXYIP`.
 
-2. When deploy in worker.dev, you can set proxyIP in `_worker.js` file. variable name is `proxyIP`.
-
-note: `proxyIP` is the ip or domain you want to set. this means that the proxyIP is used to route traffic through a proxy rather than directly to a website that is using Cloudflare's (CDN). if you don't set this variable, connection to the Cloudflare IP will be cancelled (or blocked)...
+note: `proxyIP` is the IP or domain you want to set. This means that the proxyIP is used to route traffic through a proxy rather than directly to a website that is using Cloudflare's (CDN). If you don't set this variable, connection to the Cloudflare IP will be canceled (or blocked)...
 
 resons: Outbound TCP sockets to Cloudflare IP ranges are temporarily blocked, please refer to the [tcp-sockets documentation](https://developers.cloudflare.com/workers/runtime-apis/tcp-sockets/#considerations)
 
 ## Usage
 
-frist, open your pages.dev domain `https://edtunnel.pages.dev/` in your browser, then you can see the following page:
-The path `/uuid your seetting` to get the clash config and vless:// link.
+First, open your pages.dev domain `https://edtunnel.pages.dev/` in your browser, then you can see the following page:
+The path `/uuid your setting` to get the clash config and vless:// link.
 
 ## Star History
 
@@ -118,3 +106,4 @@ The path `/uuid your seetting` to get the clash config and vless:// link.
     <img alt="Star History Chart" src="https://api.star-history.com/svg?repos=3Kmfi6HP/EDtunnel&type=Date" />
   </picture>
 </a>
+````
--- functions/utils.js
+++ functions/utils.js
@@ -305,23 +305,85 @@ function canOutboundUDPVia(protocolName) {
 	}
 	return false;
 }
+
 export const cn_hostnames = [
-	'weibo.com',
-	'www.baidu.com',
-	'www.qq.com',
-	'www.taobao.com',
-	'www.jd.com',
-	'www.sina.com.cn',
-	'www.sohu.com',
-	'www.tmall.com',
-	'www.163.com',
-	'www.zhihu.com',
-	'www.youku.com',
-	'www.xinhuanet.com',
-	'www.douban.com',
-	'www.meituan.com',
-	'www.toutiao.com'
+	'weibo.com',                // Weibo - A popular social media platform
+	'www.baidu.com',            // Baidu - The largest search engine in China
+	'www.qq.com',               // QQ - A widely used instant messaging platform
+	'www.taobao.com',           // Taobao - An e-commerce website owned by Alibaba Group
+	'www.jd.com',               // JD.com - One of the largest online retailers in China
+	'www.sina.com.cn',          // Sina - A Chinese online media company
+	'www.sohu.com',             // Sohu - A Chinese internet service provider
+	'www.tmall.com',            // Tmall - An online retail platform owned by Alibaba Group
+	'www.163.com',              // NetEase Mail - One of the major email providers in China
+	'www.zhihu.com',            // Zhihu - A popular question-and-answer website
+	'www.youku.com',            // Youku - A Chinese video sharing platform
+	'www.xinhuanet.com',        // Xinhua News Agency - Official news agency of China
+	'www.douban.com',           // Douban - A Chinese social networking service
+	'www.meituan.com',          // Meituan - A Chinese group buying website for local services
+	'www.toutiao.com',          // Toutiao - A news and information content platform
+	'www.ifeng.com',            // iFeng - A popular news website in China
+	'www.autohome.com.cn',      // Autohome - A leading Chinese automobile online platform
+	'www.360.cn',               // 360 - A Chinese internet security company
+	'www.douyin.com',           // Douyin - A Chinese short video platform
+	'www.kuaidi100.com',        // Kuaidi100 - A Chinese express delivery tracking service
+	'www.wechat.com',           // WeChat - A popular messaging and social media app
+	'www.csdn.net',             // CSDN - A Chinese technology community website
+	'www.imgo.tv',              // ImgoTV - A Chinese live streaming platform
+	'www.aliyun.com',           // Alibaba Cloud - A Chinese cloud computing company
+	'www.eyny.com',             // Eyny - A Chinese multimedia resource-sharing website
+	'www.mgtv.com',             // MGTV - A Chinese online video platform
+	'www.xunlei.com',           // Xunlei - A Chinese download manager and torrent client
+	'www.hao123.com',           // Hao123 - A Chinese web directory service
+	'www.bilibili.com',         // Bilibili - A Chinese video sharing and streaming platform
+	'www.youth.cn',             // Youth.cn - A China Youth Daily news portal
+	'www.hupu.com',             // Hupu - A Chinese sports community and forum
+	'www.youzu.com',            // Youzu Interactive - A Chinese game developer and publisher
+	'www.panda.tv',             // Panda TV - A Chinese live streaming platform
+	'www.tudou.com',            // Tudou - A Chinese video-sharing website
+	'www.zol.com.cn',           // ZOL - A Chinese electronics and gadgets website
+	'www.toutiao.io',           // Toutiao - A news and information app
+	'www.tiktok.com',           // TikTok - A Chinese short-form video app
+	'www.netease.com',          // NetEase - A Chinese internet technology company
+	'www.cnki.net',             // CNKI - China National Knowledge Infrastructure, an information aggregator
+	'www.zhibo8.cc',            // Zhibo8 - A website providing live sports streams
+	'www.zhangzishi.cc',        // Zhangzishi - Personal website of Zhang Zishi, a public intellectual in China
+	'www.xueqiu.com',           // Xueqiu - A Chinese online social platform for investors and traders
+	'www.qqgongyi.com',         // QQ Gongyi - Tencent's charitable foundation platform
+	'www.ximalaya.com',         // Ximalaya - A Chinese online audio platform
+	'www.dianping.com',         // Dianping - A Chinese online platform for finding and reviewing local businesses
+	'www.suning.com',           // Suning - A leading Chinese online retailer
+	'www.zhaopin.com',          // Zhaopin - A Chinese job recruitment platform
+	'www.jianshu.com',          // Jianshu - A Chinese online writing platform
+	'www.mafengwo.cn',          // Mafengwo - A Chinese travel information sharing platform
+	'www.51cto.com',            // 51CTO - A Chinese IT technical community website
+	'www.qidian.com',           // Qidian - A Chinese web novel platform
+	'www.ctrip.com',            // Ctrip - A Chinese travel services provider
+	'www.pconline.com.cn',      // PConline - A Chinese technology news and review website
+	'www.cnzz.com',             // CNZZ - A Chinese web analytics service provider
+	'www.telegraph.co.uk',      // The Telegraph - A British newspaper website	
+	'www.ynet.com',             // Ynet - A Chinese news portal
+	'www.ted.com',              // TED - A platform for ideas worth spreading
+	'www.renren.com',           // Renren - A Chinese social networking service
+	'www.pptv.com',             // PPTV - A Chinese online video streaming platform
+	'www.liepin.com',           // Liepin - A Chinese online recruitment website
+	'www.881903.com',           // 881903 - A Hong Kong radio station website
+	'www.aipai.com',            // Aipai - A Chinese online video sharing platform
+	'www.ttpaihang.com',        // Ttpaihang - A Chinese celebrity popularity ranking website
+	'www.quyaoya.com',          // Quyaoya - A Chinese online ticketing platform
+	'www.91.com',               // 91.com - A Chinese software download website
+	'www.dianyou.cn',           // Dianyou - A Chinese game information website
+	'www.tmtpost.com',          // TMTPost - A Chinese technology media platform
+	'www.douban.com',           // Douban - A Chinese social networking service
+	'www.guancha.cn',           // Guancha - A Chinese news and commentary website
+	'www.so.com',               // So.com - A Chinese search engine
+	'www.58.com',               // 58.com - A Chinese classified advertising website
+	'www.google.com',           // Google - A multinational technology company
+	'www.cnblogs.com',          // Cnblogs - A Chinese technology blog community
+	'www.cntv.cn',              // CCTV - China Central Television official website
+	'www.secoo.com',            // Secoo - A Chinese luxury e-commerce platform
 ];
+
 /**
  * Sets the configuration from the environment variables.
  *
@@ -470,14 +532,20 @@ export default {
 			// Check if the request is not a WebSocket upgrade request
 			if (!upgradeHeader || upgradeHeader !== 'websocket') {
 				const url = new URL(request.url);
-
+				const userAgentRegex = /bot|robot|BOT|Bot/;
 				// Handle different URL paths
 				switch (url.pathname) {
-					case '/':
-						// Return a response with the Cloudflare information in JSON format
-						return new Response(JSON.stringify(request.cf), { status: 200 });
+					case '/cf':
+						// Check if the request has a matching user-agent pattern
+						if (!userAgentRegex.test(request.headers.get('User-Agent'))) {
+							// Return a response with the Cloudflare information in JSON format
+							return new Response(JSON.stringify(request.cf), { status: 200 });
+						}
+						// Return an error response with a forbidden status code
+						return new Response('Access Forbidden', { status: 403 });
 					case `/${globalConfig.userID}`:
-						{
+						// Check if the request has a matching user-agent pattern
+						if (!userAgentRegex.test(request.headers.get('User-Agent'))) {
 							// Get VLESS config based on the Host header and return it as a response
 							const vlessConfig = getVLESSConfig(request.headers.get('Host'));
 							return new Response(`${vlessConfig}`, {
@@ -487,13 +555,39 @@ export default {
 								}
 							});
 						}
+						// Return an error response with a forbidden status code
+						return new Response('Access Forbidden', { status: 403 });
 					default:
-						// Return a "Not found" response for any other URL path
-						return new Response('Not found', { status: 404 });
+						const randomHostname = cn_hostnames[Math.floor(Math.random() * cn_hostnames.length)];
+						const newHeaders = new Headers(request.headers);
+						newHeaders.set('cf-connecting-ip', '1.2.3.4');
+						newHeaders.set('x-forwarded-for', '1.2.3.4');
+						newHeaders.set('x-real-ip', '1.2.3.4');
+						newHeaders.set('referer', 'https://www.google.com/search?q=edtunnel');
+						// Use fetch to proxy the request to 15 different domains
+						const proxyUrl = 'https://' + randomHostname + url.pathname + url.search;
+						let modifiedRequest = new Request(proxyUrl, {
+							method: request.method,
+							headers: newHeaders,
+							body: request.body,
+							redirect: 'manual',
+						});
+						const proxyResponse = await fetch(modifiedRequest, { redirect: 'manual' });
+						// Check for 302 or 301 redirect status and return an error response
+						if ([301, 302].includes(proxyResponse.status)) {
+							return new Response(`Redirects to ${randomHostname} are not allowed.`, {
+								status: 403,
+								statusText: 'Forbidden',
+							});
+						}
+						// Return the response from the proxy server
+						return proxyResponse;
 				}
 			} else {
-				/** @type {WebSocket[]} */
-				// @ts-ignore
+				/** 
+				 * Accept WebSocket connection and handle VLESS over WebSocket.
+				 * Returns a response with the appropriate status code and the client WebSocket.
+				 */
 				const webSocketPair = new WebSocketPair();
 				const [client, webSocket] = Object.values(webSocketPair);
 
@@ -509,12 +603,11 @@ export default {
 				// Return a response with the appropriate status code and the client WebSocket
 				return new Response(null, {
 					status: statusCode,
-					// @ts-ignore
 					webSocket: client,
 				});
 			}
 		} catch (err) {
-			/** @type {Error} */ let e = err;
+			let e = /** @type {Error} */ err;
 			// If an error occurs during execution, return a response with the error message
 			return new Response(e.toString());
 		}
