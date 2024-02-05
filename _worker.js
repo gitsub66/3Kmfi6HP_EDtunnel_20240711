8e6ff3398c2a3eee48103069111f65686fecf7ee
satriayy <79690213+satriayy@users.noreply.github.com>
2024-02-05 15:55:18 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f2572832-ecf3-4742-b20a-978cfb2e28c8';
 
-const proxyIPs = ['satriadyit.eu.org'];
+const proxyIPs = ['root.satriadyit.eu.org'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
