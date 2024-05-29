7e6f4cf81ccec614fc2585ca92f013ea5d18f6c6
muchliz <85377032+muchliz@users.noreply.github.com>
2024-05-29 20:31:04 +0700

    _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f19a9e5b-9dcb-4a09-896d-3e283550006f';
 
 const proxyIPs = ['8.219.235.164'];
 
