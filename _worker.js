7d3c4efec09bbfc3040f8bddf4aa941898fb6836
Noysz Fourfect <126023454+Wskas4@users.noreply.github.com>
2024-05-30 17:43:14 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let configRemarks = "NOYSZ-VLESS"
+let configRemarks = "NOYSZ-ID"
 
 const พร็อกซีไอพีs = ['52.74.101.26'];
 
