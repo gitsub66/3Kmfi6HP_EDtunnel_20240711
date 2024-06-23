8871f4a30f252ea53c41b5f0bcb1f4d31ab2b4b8
fadli <fadzvpnid@gmail.com>
2024-06-23 09:00:56 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '4819b8b0-6a33-4c6c-8d33-c0947ecbe803';
 
-const พร็อกซีไอพีs = ['72.13.122.137'];
+const พร็อกซีไอพีs = ['192.237.192.175'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
